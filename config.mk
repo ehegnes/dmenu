8233bf40dfcf4c97f48033884d67451c4ad84832
Eric Hegnes <eric.hegnes@gmail.com>
2016-07-21 11:01:45 -0400

    Add optimizations and use proper lib paths

--- config.mk
+++ config.mk
@@ -5,8 +5,8 @@ VERSION = 4.6
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -22,10 +22,18 @@ FREETYPEINC = /usr/include/freetype2
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
+# optimizations
+LTOFLAGS      = -flto=jobserver -fuse-linker-plugin -fno-trapping-math -fno-signed-zeros
+GRAPHITEFLAGS = -ftree-vectorize -floop-interchange -floop-strip-mine -floop-block
+RELEASEFLAGS  = -march=native -mtune=native -Ofast -pipe -g0 -funroll-loops
+
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS} ${LTOFLAGS} ${GRAPHITEFLAGS} ${RELEASEFLAGS}
 LDFLAGS  = -s ${LIBS}
+
+# use gold linker
+LDFLAGS += -fuse-ld=gold
 
 # compiler and linker
 CC = cc
